b915778f31f03586cea0cab039aee54cf880819a
jwkim <phoongdeng@gmail.com>
2019-06-08 22:17:30 +0900

    테이블 내 리스트 확인함수 추가 및 테스트 케이스에 적용

--- functional_test.py
+++ functional_test.py
@@ -13,6 +13,11 @@ class NewVisitiorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get('http://localhost:8000')
 
@@ -26,14 +31,16 @@ class NewVisitiorTest(unittest.TestCase):
             '작업 아이템 입력')
 
         inputbox.send_keys('공작깃털 사기')
-
         inputbox.send_keys(Keys.ENTER)
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn(
-            '1: 공작깃털 사기', 
-            [row.text for row in rows])
+        self.check_for_row_in_list_table('1: 공작깃털 사기')
+
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('공작깃털을 이용해서 그물 만들기')
+        inputbox.send_keys(Keys.ENTER)
+
+        self.check_for_row_in_list_table('2: 공작깃털을 이용해서 그물 만들기')
+        self.check_for_row_in_list_table('1: 공작깃털 사기')
 
         self.fail('Finish the test !!')
 
