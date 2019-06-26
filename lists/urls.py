35b17ff00bff8558394aef28f4c24029fc74e40e
jwkim <phoongdeng@gmail.com>
2019-06-26 21:32:49 +0900

    기존 리스트에 아이템 추가하는 신규 URL/뷰 추가

--- lists/urls.py
+++ lists/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home_page),
-    path('lists/the-only-list-in-the-world/', views.view_list, name='view_list'),
+    path('lists/<int:list_id>/', views.view_list, name='view_list'),
+    path('lists/<int:list_id>/add_item', views.add_item, name='add_item'),
     path('lists/new', views.new_list, name='new_list'),
 ]
