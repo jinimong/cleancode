d5317eaa6ee1f298453a2261ba3175132f33fea4
jwkim <phoongdeng@gmail.com>
2019-06-30 23:54:52 +0900

    부트스트랩으로 레이아웃 개선

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,9 +1,10 @@
 from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 
-class NewVisitiorTest(LiveServerTestCase):
+class NewVisitiorTest(StaticLiveServerTestCase):
     
     def setUp(self):
         self.browser = webdriver.Chrome('chromedriver')
